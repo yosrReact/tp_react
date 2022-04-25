1aeeeac9837773d510719b0fa758cdde70e49d08
Yosr Naija <yosr.naija@live.com>
2022-04-25 09:01:24 +0100

    fix const

--- cypress/e2e/todos.spec.js
+++ cypress/e2e/todos.spec.js
@@ -15,10 +15,8 @@ describe("Name of the group", () => {
   // })
   it("login", function () {
     // destructuring assignment of the this.currentUser object
-    const { email, password } = {
-      email: "yosrisamm1@gmail.com",
-      password: "123456",
-    } //{this.currentUser
+    const email = "yosrisamm1@gmail.com"
+    const password = "123456"
 
     cy.visit("/login")
     cy.get(":nth-child(1) > input").type(email)
@@ -30,17 +28,28 @@ describe("Name of the group", () => {
   it("successfully loads", () => {
     // cy.visit("http://localhost:3000")
     // cy.visit("/")
-    // cy.wait(3000)
     // cy.get("a").click()
     cy.contains(/tasks/i).click()
     cy.url().should("include", "/task-page")
-    // cy.get('[aria-label="Title"]').clear().type("learn react")
+    cy.wait(1000)
     cy.findByLabelText(/title/i).clear().type("learn react")
     // cy.get('[data-testid="submit"]').click()
     cy.findByTestId("submit").click()
-    cy.get('[aria-label="Title"]').should("have.value", "")
+
     // cy.get(".task:last-child .title").should("contain", "learn react")
     cy.get(".task:last-child .title").should("have.text", "learn react")
+    cy.get('[aria-label="Title"]').should("have.value", "")
+
+    // cy.url().should("include", "/task-page")
+    // cy.wait(3000)
+    // cy.get("a").click()
+    // cy.contains(/tasks/i).click()
+    // // cy.get('[aria-label="Title"]').clear().type("learn react")
+    // cy.findByLabelText(/title/i).clear().type("learn react")
+    // // cy.get('[data-testid="submit"]').click()
+    // cy.findByTestId("submit").click()
+    // cy.get('[aria-label="Title"]').should("have.value", "")
+    // // cy.get(".task:last-child .title").should("contain", "learn react")
   })
   // it("should show an error message if there's an error in adding task", () => {
   //   cy.server()
